96b9d410b5b99de6362d182c5db3bf20ad73e4ed
kiranagisneha <kiranagisneha@gmail.com>
2025-12-03 17:53:30 +0530

    Update test_product.py

--- test_product.py
+++ test_product.py
@@ -1,10 +1,11 @@
+from product import product_info
+
 def test_product_info():
     result = product_info("P101", "Laptop", 5, 60000)
     expected = (
         "Product ID: P101\n"
-        "Product Name: Laptop\n"
+        "Name: Laptop\n"
         "Quantity: 5\n"
         "Price: 60000"
     )
     assert result == expected
-
